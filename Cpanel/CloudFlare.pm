b63f8a6ae10b97faa13afab39fb86ad578bc3b13
root <root@potato.cloudflare.com>
2010-12-10 17:41:09 -0800

    initial import

---
+++ Cpanel/CloudFlare.pm
@@ -0,0 +1,320 @@
+package Cpanel::CloudFlare;
+
+# cpanel - Cpanel/CloudFlare.pm                   Copyright(c) 2010 CloudFlare, Inc.
+#                                                               All rights Reserved.
+# copyright@cloudflare.com                                     http://cloudflare.com
+# This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+use Cpanel::DnsUtils::UsercPanel ();
+use Cpanel::AdminBin             ();
+use Cpanel::Locale               ();
+use Cpanel::Logger               ();
+use Cpanel::UrlTools             ();
+use Cpanel::SocketIP             ();
+use Cpanel::Encoder::URI         ();
+use Cpanel::CustInfo ();
+use Cpanel::AcctUtils ();
+
+use JSON::Syck                   ();
+
+my $logger = Cpanel::Logger->new();
+my $locale;
+my $cf_config_file = "/usr/local/cpanel/etc/cloudflare.json";
+my $cf_host_key;
+my $cf_host_name;
+my $cf_host_uri;
+my $cf_host_port;
+my $cf_host_prefix;
+
+
+## Initialize vars here.
+sub CloudFlare_init { 
+    my $data = JSON::Syck::LoadFile($cf_config_file);
+    $cf_host_key = $data->{"host_key"};
+    $cf_host_name = $data->{"host_name"};
+    $cf_host_uri = $data->{"host_uri"};
+    $cf_host_port = $data->{"host_port"};
+    $cf_host_prefix = $data->{"host_prefix"};
+}
+
+
+# Can only be called with json or xml api because it uses
+# a non-standard return
+sub api2_user_create {
+    my %OPTS = @_;
+
+    if (!$cf_host_key) {
+        $logger->info("Missing cf_host_key! Define this in $cf_config_file.");
+        return [];
+    }
+
+    # Use a random string as a password.
+    my $password = crypt(int(rand(10000000)), time);
+    $logger->info("Createing Cloudflare user for " . $OPTS{"email"} . " -- " . $password);
+   
+    ## Otherwise, try to create this user.
+    my $args = {
+        "host" => $cf_host_name,
+        "uri" => $cf_host_uri,
+        "port" => $cf_host_port,
+        "query" => {
+            "act" => "user_create",
+            "host_key" => $cf_host_key,
+            "cloudflare_email" => $OPTS{"email"},
+            "cloudflare_pass" => $password,
+            "cloudflare_username" => $OPTS{"user"}
+        },
+    };
+
+    my $result = __https_post_req->($args);  
+    return JSON::Syck::Load($result);
+}
+
+# Can only be called with json or xml api because it uses
+# a non-standard return
+sub api2_user_lookup {
+    my %OPTS = @_;
+
+    if (!$cf_host_key) {
+        $logger->info("Missing cf_host_key! Define this in $cf_config_file.");
+        return [];
+    }
+
+    ## Otherwise, try to log this user in.
+    my $login_args = {
+        "host" => $cf_host_name,
+        "uri" => $cf_host_uri,
+        "port" => $cf_host_port,
+        "query" => {
+            "act" => "user_lookup",
+            "host_key" => $cf_host_key,
+            "cloudflare_email" => $OPTS{"email"}
+        },
+    };
+
+    my $result = __https_post_req->($login_args);
+    return JSON::Syck::Load($result);
+}
+
+# Can only be called with json or xml api because it uses
+# a non-standard return
+sub api2_zone_set {
+    my %OPTS = @_;
+
+    if (!$cf_host_key || !$cf_host_prefix) {
+        $logger->info("Missing cf_host_key or $cf_host_prefix! Define these in $cf_config_file.");
+        return [];
+    }
+
+    my $domain = ".".$OPTS{"zone_name"}.".";
+    my $subs = $OPTS{"subdomains"};
+    $subs =~ s/${domain}//g;
+
+    ## Unpack the mapping from recs to lines (ugg, this is SOOO BAAD)
+    my $recs2lines = JSON::Syck::Load($OPTS{"cf_recs"});
+    
+    ## Set up the zone_set args.
+    my $login_args = {
+        "host" => $cf_host_name,
+        "uri" => $cf_host_uri,
+        "port" => $cf_host_port,
+        "query" => {
+            "act" => "zone_set",
+            "host_key" => $cf_host_key,
+            "user_key" => $OPTS{"user_key"},
+            "zone_name" => $OPTS{"zone_name"},
+            "resolve_to" => $cf_host_prefix . "." . $OPTS{"zone_name"},
+            "subdomains" => $subs
+        },
+    };
+
+    if (!$subs) {
+        $login_args->{"query"}->{"act"} = "zone_delete";
+        $logger->info("Deleteing " . $domain);
+    } else {   
+        $logger->info("For " . $domain);
+        $logger->info("Updating " . $subs);
+    }
+
+    my $result = JSON::Syck::Load(__https_post_req->($login_args));
+    
+    ## Args for updating local DNS.
+    my %zone_args = ("domain" => $OPTS{"zone_name"},
+                     "class" => "IN",
+                     "type" => "CNAME",
+                     "name" => $cf_host_prefix,
+                     "ttl" => 1400,
+                     "cname" => $OPTS{"zone_name"},
+        );
+
+    ## If we get an error, do nothing and return the error to the user.
+    if ($result->{"result"} eq "error") {
+        $logger->info("ERROR: " . $result->{"msg"});
+    } else {
+        ## Otherwise, update the dns for this zone.
+        my $dom = ".".$OPTS{"zone_name"};
+             
+        ## First Make sure that the resolve to is set.
+        my $res = Cpanel::AdminBin::adminfetchnocache( 'zone', '', 'ADD', 'storable', $OPTS{"zone_name"},
+                                                    __serialize_request( \%zone_args ) ); 
+        
+        ## If there's a delete, remove this record from being CF.
+        if ($OPTS{"old_line"}) {
+            $zone_args{"line"} = $OPTS{"old_line"};
+            $zone_args{"name"} = $OPTS{"old_rec"};
+            $zone_args{"name"} =~ s/$domain//g;
+            $zone_args{"cname"} = $OPTS{"zone_name"};
+            $res = Cpanel::AdminBin::adminfetchnocache( 'zone', '', 'EDIT', 'storable', $OPTS{"zone_name"},
+                                                        __serialize_request( \%zone_args ) );
+        }
+        
+        ## Now, go ahead and update all of the CF enabled recs.
+        $logger->info(JSON::Syck::Dump($result));
+   
+        foreach $ft (keys %{$result->{"response"}->{"forward_tos"}}) {
+            $zone_args{"line"} = $recs2lines->{$ft."."};
+            $zone_args{"name"} = $ft;
+            $zone_args{"name"} =~ s/$dom//g;
+            $zone_args{"cname"} = $result->{"response"}->{"forward_tos"}->{$ft};
+            $res = Cpanel::AdminBin::adminfetchnocache( 'zone', '', 'EDIT', 'storable', $OPTS{"zone_name"},
+                                                        __serialize_request( \%zone_args ) );
+        }
+    }
+    return $result;
+}
+
+sub api2_fetchzone {
+    my $raw = __fetchzone(@_);
+    my $results = [];
+    my %OPTS    = @_;
+    my $domain = $OPTS{'domain'}.".";
+
+    # @TODO -- Add support for A recs here
+    foreach my $res (@{$raw->{"record"}}) {   
+        if (($res->{"type"} eq "CNAME") && 
+            ($res->{"name"} !~ /(mail)|(cpanel)|(whm)|(ftp)|(localhost)|($cf_host_prefix)/) &&
+            ($res->{"name"} ne $domain)){
+
+            if ($res->{"cname"} =~ /cdn.cloudflare.net$/) {
+                $res->{"cloudflare"} = 1;
+            } else {
+                $res->{"cloudflare"} = 0;
+            }
+            push @$results, $res; 
+        }
+    }
+    return $results;
+}
+
+sub api2 {
+    my $func = shift;
+
+    my %API;
+
+    $API{'user_create'}{'func'}                        = 'api2_user_create';
+    $API{'user_create'}{'engine'}                      = 'hasharray';
+    $API{'user_lookup'}{'func'}                        = 'api2_user_lookup';
+    $API{'user_lookup'}{'engine'}                      = 'hasharray';
+    $API{'zone_set'}{'func'}                           = 'api2_zone_set';
+    $API{'zone_set'}{'engine'}                         = 'hasharray';
+    $API{'fetchzone'}{'func'}                          = 'api2_fetchzone';
+    $API{'fetchzone'}{'engine'}                        = 'hasharray';
+
+    return ( \%{ $API{$func} } );
+}
+
+########## Internal Functions Defined Below #########
+
+sub __fetchzone {
+    my %OPTS    = @_;
+    my $domain  = $OPTS{'domain'};
+    my $results = Cpanel::AdminBin::adminfetchnocache( 'zone', '', 'FETCH', 'storable', $domain, ($OPTS{'customonly'} ? 1 : 0)  );
+
+    if ( ref $results->{'record'} eq 'ARRAY' ) {
+        for(0 .. $#{ $results->{'record'} }) {
+            $results->{'record'}->[$_]->{'record'} = ($results->{'record'}->[$_]->{'address'} || $results->{'record'}->[$_]->{'cname'} || $results->{'record'}->[$_]->{'txtdata'});
+            $results->{'record'}->[$_]->{'line'} = ($results->{'record'}->[$_]->{'Line'});
+        }
+        foreach my $key ( keys %KEYMAP ) {
+            if ( exists $OPTS{$key} && defined $OPTS{$key} ) {
+                my %MULTITYPES = map { $_ => undef } split(/[\|\,]/, $OPTS{$key});
+                @{ $results->{'record'} } = grep { exists $MULTITYPES{$_->{ $KEYMAP{$key} }} } @{ $results->{'record'} };
+            }
+        }
+    }
+
+    return $results;
+}
+
+sub __https_post_req {
+    my ( $args_hr ) = @_;
+
+    my $query = $args_hr->{'query'};
+    if ( ref $args_hr->{'query'} eq 'HASH' ) {
+        $query = '';
+        foreach my $key ( keys %{ $args_hr->{'query'} } ) {
+            if ( ref $args_hr->{'query'}{$key} eq 'ARRAY' ) {
+                for my $val ( @{ $args_hr->{'query'}{$key} } ) {
+                    $query .=
+                      $query
+                      ? "&$key=" . Cpanel::Encoder::URI::uri_encode_str($val)
+                      : "$key=" . Cpanel::Encoder::URI::uri_encode_str($val);
+                }
+            }
+            else {
+                $query .=
+                  $query
+                  ? "&$key=" . Cpanel::Encoder::URI::uri_encode_str( $args_hr->{'query'}{$key} )
+                  : "$key=" . Cpanel::Encoder::URI::uri_encode_str( $args_hr->{'query'}{$key} );
+            }
+        }
+    }
+
+    my $postdata_len = length($query);
+
+    my $proto = getprotobyname('tcp');
+    return unless defined $proto;
+
+    socket( my $socket_fh, &Socket::AF_INET, &Socket::SOCK_STREAM, $proto );
+    return unless $socket_fh;
+
+    my $iaddr = gethostbyname($args_hr->{'host'});
+    my $port = $args_hr->{'port'};
+    
+    return unless ( defined $iaddr && defined $port );
+
+    my $sin = Socket::sockaddr_in( $port, $iaddr );
+    return unless defined $sin;
+    
+    my $result = "";
+    if ( connect( $socket_fh, $sin ) ) {
+
+        send $socket_fh, "POST /$args_hr->{'uri'} HTTP/1.0\nContent-Length: $postdata_len\nContent-Type: application/x-www-form-urlencoded\nHost: $args_hr->{'host'}\n\n$query", 0;
+
+        my $in_header = 1;
+        while (<$socket_fh>) {
+            if ( /^\n$/ || /^\r\n$/ || /^$/ ) {
+                $in_header = 0;
+                next;
+            }
+            
+            if (!$in_header) {
+                $result .=  $_;
+            }
+        }
+    }
+
+    close $socket_fh;
+    return $result;
+}
+
+sub __serialize_request {
+    my $opt_ref = shift;
+    my @KEYLIST;
+    foreach my $opt ( keys %$opt_ref ) {
+        push @KEYLIST, Cpanel::Encoder::URI::uri_encode_str($opt) . '=' . Cpanel::Encoder::URI::uri_encode_str( $opt_ref->{$opt} );
+    }
+    return join( '&', @KEYLIST );
+}
+
+1;
---
+++ base/frontend/x3/js2/cloudflare/crap.js
@@ -0,0 +1,307 @@
+
+var VALID = [];
+var CF_RECS = {};
+
+var signup_to_cf = function() {
+
+    var tos = YAHOO.util.Dom.get("USER_tos").checked;
+    if (!tos) {
+		CPANEL.widgets.status_bar("add_USER_status_bar", "error", CPANEL.lang.Error, "Please agree to the Terms of Service before continuing.");
+        return false;
+    }
+
+
+
+    // build the call
+	var email = YAHOO.util.Dom.get("USER_email").value;
+	var user = YAHOO.util.Dom.get("USER_user").value;
+	var api2_call = {
+		"cpanel_jsonapi_version" : 2,
+		"cpanel_jsonapi_module" : "CloudFlare",
+		"cpanel_jsonapi_func" : "user_create",
+        "user" : user,
+        "email" : email,
+	};
+    
+    // callback
+    var callback = {
+        success : function(o) {
+			try {
+				var data = YAHOO.lang.JSON.parse(o.responseText);
+				if (data.cpanelresult.error) {
+                    YAHOO.util.Dom.get("add_USER_record_status").innerHTML = "";
+					CPANEL.widgets.status_bar("add_USER_status_bar", "error", CPANEL.lang.Error, data.cpanelresult.error);
+				}
+                else if (data.cpanelresult.data[0].result == 'success') {
+                    YAHOO.util.Dom.get("add_USER_record_status").innerHTML = "";
+					CPANEL.widgets.status_bar("add_USER_status_bar", "success", "Welcome to CloudFlare", "Generate a CloudFlare password <a href=\"https://www.cloudflare.com/forgot-password.html\">here</a>. Your CloudFlare email is curently set to " + email + ". Click <a href=\"\">here</a> to continue.");
+                    // After 10 sec, reload the page
+                    setTimeout('window.location.reload(true)', 10000);
+				}
+				else {
+                    YAHOO.util.Dom.get("add_USER_record_status").innerHTML = '';
+					CPANEL.widgets.status_bar("add_USER_status_bar", "error", CPANEL.lang.Error, data.cpanelresult.data[0].msg.replace(/\\/g, ""));
+				}
+			}
+			catch (e) {
+                YAHOO.util.Dom.get("add_USER_record_status").innerHTML = "";
+				CPANEL.widgets.status_bar("add_USER_status_bar", "error", CPANEL.lang.json_error, CPANEL.lang.json_parse_failed);
+			}
+        },
+        failure : function(o) {
+			YAHOO.util.Dom.setStyle("add_USER_record_button", "display", "block");
+			YAHOO.util.Dom.get("add_USER_record_status").innerHTML = "";
+			CPANEL.widgets.status_bar("add_USER_status_bar", "error", CPANEL.lang.ajax_error, CPANEL.lang.ajax_try_again);
+        }
+    };
+    
+    // send the request
+    YAHOO.util.Connect.asyncRequest('GET', CPANEL.urls.json_api(api2_call), callback, '');
+    
+    YAHOO.util.Dom.setStyle("add_USER_record_button", "display", "none");
+    YAHOO.util.Dom.get("add_USER_record_status").innerHTML = CPANEL.icons.ajax + " " + "Creating Your CloudFlare Account";
+};
+
+var reset_form = function(type) {
+    YAHOO.util.Dom.setStyle("add_" + type + "_record_button", "display", "block");
+    YAHOO.util.Dom.get("add_" + type + "_record_status").innerHTML = '';
+    
+	YAHOO.util.Dom.get(type + "_name").value = "";
+    
+    VALID[type + "_name"].clear_messages();
+};
+
+var add_validation = function() {
+     
+};
+
+var toggle_domain = function() {
+	var domain = YAHOO.util.Dom.get("domain").value;
+	if (domain == "_select_") {
+		$("#add_record_and_zone_table").slideUp(CPANEL.JQUERY_ANIMATION_SPEED);
+	}
+	else {
+		$("#add_record_and_zone_table").slideDown(CPANEL.JQUERY_ANIMATION_SPEED);
+		update_user_records_table();
+	}
+	reset_form("CNAME");
+};
+
+    var update_zones = function(rec_num, orig_state, old_rec, old_line) {
+    var callback = {
+        success : function(o) {
+            try {
+                var data = YAHOO.lang.JSON.parse(o.responseText);
+                if (data.cpanelresult.error) {
+                    YAHOO.util.Dom.get("cloudflare_table_edit_" + rec_num).innerHTML 
+                        = '<img src="https://www.cloudflare.com/images/icons-custom/solo_cloud' 
+                        + orig_state + '-55x25.png" />';
+                    CPANEL.widgets.status_bar("status_bar_" + rec_num, "error", 
+                                              CPANEL.lang.json_error, CPANEL.lang.json_parse_failed);
+                } else if (data.cpanelresult.data[0].result == "error") {            
+                    YAHOO.util.Dom.get("cloudflare_table_edit_" + rec_num).innerHTML 
+                        = '<img src="https://www.cloudflare.com/images/icons-custom/solo_cloud' 
+                        + orig_state + '-55x25.png" />';
+                    CPANEL.widgets.status_bar("status_bar_" + rec_num, "error", 
+                                              CPANEL.lang.json_error, 
+                                              data.cpanelresult.data[0].msg.replace(/\\/g, ""));
+				}
+				else {
+                    update_user_records_table();
+			    }
+			}
+			catch (e) {
+                YAHOO.util.Dom.get("cloudflare_table_edit_" + rec_num).innerHTML 
+                    = '<img src="https://www.cloudflare.com/images/icons-custom/solo_cloud' 
+                    + orig_state + '-55x25.png" />';
+                CPANEL.widgets.status_bar("status_bar_" + rec_num, "error", 
+                                          CPANEL.lang.json_error, CPANEL.lang.json_parse_failed);
+			}
+        },
+        failure : function(o) {            
+            YAHOO.util.Dom.get("cloudflare_table_edit_" + rec_num).innerHTML 
+                = '<img src="https://www.cloudflare.com/images/icons-custom/solo_cloud' 
+                + orig_state + '-55x25.png" />';
+            YAHOO.util.Dom.get("status_bar_" + rec_num).innerHTML = '<div style="padding: 20px">' + CPANEL.icons.error + " " + CPANEL.lang.ajax_error + ": " + CPANEL.lang.ajax_try_again + "</div>";
+        }
+    };
+    
+    var cf_zones = [];
+    for (key in CF_RECS) {
+        if (CF_RECS[key]) {
+            cf_zones.push(key);
+        }
+    }
+
+    // send the AJAX request
+    var api2_call = {
+		"cpanel_jsonapi_version" : 2,
+		"cpanel_jsonapi_module" : "CloudFlare",
+		"cpanel_jsonapi_func" : "zone_set",
+		"zone_name" : YAHOO.util.Dom.get("domain").value,
+        "user_key" : USER_ID,
+        "subdomains" : cf_zones.join(","),
+        "cf_recs" : YAHOO.lang.JSON.stringify(CF_RECS),
+	};
+
+    if (old_rec) {
+        api2_call["old_rec"] = old_rec;
+        api2_call["old_line"] = old_line;
+    }   
+
+    YAHOO.util.Connect.asyncRequest('GET', CPANEL.urls.json_api(api2_call), callback, '');
+    YAHOO.util.Dom.get("cloudflare_table_edit_" + rec_num).innerHTML = '<div style="padding: 20px">' + CPANEL.icons.ajax + " " + CPANEL.lang.ajax_loading + "</div>";
+};
+
+// Removes the given Rec from CF
+var toggle_record_off = function(rec_num, old_rec, line) {
+    CF_RECS[old_rec] = 0;
+    update_zones(rec_num, "", old_rec, line);
+};
+
+// Adds the given Rec to CF
+var toggle_record_on = function(rec_num, new_rec, line) {
+    CF_RECS[new_rec] = line;
+    update_zones(rec_num, "_off");
+};
+
+// Builds the Zone List.
+var build_dnszone_table_markup = function(records) {
+	// set the initial row stripe
+	var row_toggle = 'rowA';
+
+	// loop through the dnszone accounts and build the table
+	var html  = '<table id="table_dns_zone" class="dynamic_table" border="0" cellspacing="0" cellpadding="0">';
+        html += '<tr class="dt_header_row">';
+        html += 	'<th>Type</th>';
+        html += 	'<th>Name</th>';
+        html += 	'<th colspan="2">Record</th>';
+        html += 	'<th>CloudFlare Status</th>';
+        html += '</tr>';
+
+	for (var i=0; i<records.length; i++) {
+         
+        // A, MX, CNAME, TXT records
+        if (records[i]['type'].match(/^(A|CNAME)$/)) {
+
+            html += '<tr id="info_row_' + i + '" class="dt_info_row ' + row_toggle + '">';
+
+            html += '<td id="name_value_' + i + '">' + records[i]['type'] + '</td>';
+            html += '<td id="type_value_' + i + '">' + records[i]['name'].substring(0, records[i]['name'].length - 1) + '</td>';
+            // A
+            if (records[i]['type'] == 'A') {
+                html += '<td colspan="2" id="value_value_hehe_' + i + '">points to ' + records[i]['address'] + '</td>';
+            } 
+            // CNAME
+            else if (records[i]['type'] == 'CNAME') {
+                html += '<td colspan="2" id="value_value_hehe_' + i + '">points to ' + records[i]['cname'] + '</td>';
+            } 		
+
+		    // action links
+            html += '<td>';
+
+            if (records[i]['cloudflare'] == 1) {
+                html +=		'<span class="action_link" id="cloudflare_table_edit_' + i 
+                    + '" onclick="toggle_record_off(' + i + ', \'' + records[i]['name'] + '\', '
+                    + records[i]['line']+' )"><img src="https://www.cloudflare.com/images/icons-custom/solo_cloud-55x25.png" /></span>';
+                // And add the zone to our list of CF zones.
+                CF_RECS[records[i]['name']] = records[i]['line'];
+		    } else {
+                html +=		'<span class="action_link" id="cloudflare_table_edit_' + i 
+                    + '" onclick="toggle_record_on(' + i + ', \'' + records[i]['name'] + '\', '
+                    + records[i]['line']+' )"><img src="https://www.cloudflare.com/images/icons-custom/solo_cloud_off-55x25.png" /></span>';
+            }
+            html += '</td>';
+		    
+            html += '</tr>';
+
+            html += '<tr id="module_row_' + i + '" class="dt_module_row ' + row_toggle + '"><td colspan="7">';
+            html += 	'<div id="dnszone_table_edit_div_' + i + '" class="dt_module"></div>';
+            html += 	'<div id="dnszone_table_delete_div_' + i + '" class="dt_module"></div>';
+		    html += 	'<div id="status_bar_' + i + '" class="cjt_status_bar"></div>';
+            html += '</td></tr>';
+        }
+
+        // alternate row stripes
+		row_toggle = (row_toggle == 'rowA') ? row_toggle = 'rowB' : 'rowA';
+	}
+	html += '</table>';
+	return html;
+};
+
+var update_user_records_table = function() {
+    var callback = {
+        success : function(o) {
+            try {
+				var data = YAHOO.lang.JSON.parse(o.responseText);
+				if (data.cpanelresult.error) {
+                    YAHOO.util.Dom.get("user_records_div").innerHTML = '<div style="padding: 20px">' + CPANEL.icons.error + " " + CPANEL.lang.ajax_error + ": " + CPANEL.lang.ajax_try_again + "</div>";
+				}
+				else if (data.cpanelresult.data) {                    
+                    var html = build_dnszone_table_markup(data.cpanelresult.data);
+                    YAHOO.util.Dom.get("user_records_div").innerHTML = html;
+				}
+				else {
+                    YAHOO.util.Dom.get("user_records_div").innerHTML = '<div style="padding: 20px">' + CPANEL.icons.error + " " + CPANEL.lang.ajax_error + ": " + CPANEL.lang.ajax_try_again + "</div>";
+			    }
+			}
+			catch (e) {
+				CPANEL.widgets.status_bar("add_CNAME_status_bar", "error", CPANEL.lang.json_error, CPANEL.lang.json_parse_failed);
+			}
+        },
+        failure : function(o) {
+            YAHOO.util.Dom.get("user_records_div").innerHTML = '<div style="padding: 20px">' + CPANEL.icons.error + " " + CPANEL.lang.ajax_error + ": " + CPANEL.lang.ajax_try_again + "</div>";
+        }
+    };
+    
+    // send the AJAX request
+    var api2_call = {
+		"cpanel_jsonapi_version" : 2,
+		"cpanel_jsonapi_module" : "CloudFlare",
+		"cpanel_jsonapi_func" : "fetchzone",
+		"domain" : YAHOO.util.Dom.get("domain").value
+	};
+    
+    YAHOO.util.Connect.asyncRequest('GET', CPANEL.urls.json_api(api2_call), callback, '');
+    YAHOO.util.Dom.get("user_records_div").innerHTML = '<div style="padding: 20px">' + CPANEL.icons.ajax + " " + CPANEL.lang.ajax_loading + "</div>";
+};
+
+var init_page = function() {
+
+    // New signups
+    var oUserSub = document.getElementById("USER_submit");
+    YAHOO.util.Event.addListener(oUserSub, "click", signup_to_cf);
+
+    // change domain
+	YAHOO.util.Event.on("domain", "change", toggle_domain);
+    
+    add_validation();
+  
+    // load the table
+	if (YAHOO.util.Dom.get("domain").value != "_select_") {
+		update_user_records_table();
+	} 
+    
+};
+
+YAHOO.util.Event.onDOMReady(init_page);
+
+//this style rule must be independent of external style sheets
+(function() {
+    var _stylesheet = [
+        //other rules can be added to this array
+        ['div.dt_module', 'display:none']
+    ];
+    var inserter;
+    var first_stylesheet = document.styleSheets[0];
+    if ('insertRule' in first_stylesheet) { //W3C DOM
+        _stylesheet.forEach( function(rule) {
+            first_stylesheet.insertRule( rule[0] + ' {'+rule[1]+'}', 0 );
+        } );
+    }
+    else { //IE
+        _stylesheet.forEach( function(rule) {
+            first_stylesheet.addRule( rule[0], rule[1], 0 );
+        } );
+    }
+})();
