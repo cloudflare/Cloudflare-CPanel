5ad78acc3b84f128ed89d431a32da4b5b58fa72f
Jerome Chen <jerome@cloudflare.com>
2013-01-22 16:49:26 -0800

    Updated update script for cron

--- cloudflare/bin/cloudflare_update.sh
+++ cloudflare/bin/cloudflare_update.sh
@@ -5,32 +5,45 @@ cd /usr/local/cpanel
 # Pull the host key
 tmp_host_key=`cat etc/cloudflare.json | grep host_key | cut -d "\"" -f 4`
 host_key=""
-if [ -e /usr/local/cpanel/bin/apikeywrap ]; then
-    host_key=`/usr/local/cpanel/bin/apikeywrap $tmp_host_key`
+
+if [ -e /root/.cpanel/datastore/cf_api ]; then
+    host_key=`cat /root/.cpanel/datastore/cf_api`
 else 
     host_key=$tmp_host_key
 fi
+
 host_formal_name=`cat etc/cloudflare.json | grep host_formal_name | cut -d "\"" -f 4`
 if [ "$host_key" == "" ]; then
     echo "error -- can not find a valid host key"
     exit 10
 fi
 
-# Pull the latest version down
-dir=`perl Cpanel/CloudFlare.pm check | tail -1`
 
-# If a new file exists, install it
-if [ ! -d cloudflare_tmp ] && [ -e "$dir/cloudflare.tar.gz" ] && [ `stat -c %u $dir/cloudflare.tar.gz` -eq 0 ]; then
-    mkdir cloudflare_tmp
-    mv /$dir/cloudflare.tar.gz cloudflare_tmp/
-    cd cloudflare_tmp
-    tar -zxf cloudflare.tar.gz
-    mv */cloudflare .
-    cd cloudflare
-    ./install_cf $host_key mod_cf "${host_formal_name}"
+installed_version=`cat etc/cloudflare.json | grep version | cut -d "\"" -f 4`
+current_version=`curl -s https://api.cloudflare.com/host-gw.html -d "act=cpanel_info" -d "host_key=$host_key" | python -mjson.tool | grep cpanel_latest | cut -d "\"" -f 4`
+current_version_sha=`curl -s https://api.cloudflare.com/host-gw.html -d "act=cpanel_info" -d "host_key=$host_key" | python -mjson.tool | grep cpanel_sha1 | cut -d "\"" -f 4`
 
-    # Cleanup
-    cd /usr/local/cpanel
-    rm -rf $dir
-    rm -rf cloudflare_tmp
+if [[ $installed_version != $current_version ]] 
+	then
+		curl -s -k -L https://github.com/cloudflare/CloudFlare-CPanel/tarball/master > cloudflare.tar.gz
+		download_sha=`shasum cloudflare.tar.gz | awk '{print $1}'`
+		
+		if [[ $download_sha == $current_version_sha ]] 
+			then
+			# If a new file exists, install it
+			if [ ! -d cloudflare_tmp ] && [ -e "cloudflare.tar.gz" ] && [ `stat -c %u cloudflare.tar.gz` -eq 0 ]; then
+			    mkdir cloudflare_tmp
+			    mv cloudflare.tar.gz cloudflare_tmp/
+			    cd cloudflare_tmp
+			    tar -zxf cloudflare.tar.gz
+			    mv */cloudflare .
+			    cd cloudflare
+			    ./install_cf $host_key mod_cf "${host_formal_name}"
+
+			    # Cleanup
+			    cd /usr/local/cpanel
+			    rm -rf $dir
+			    rm -rf cloudflare_tmp
+			fi
+		fi
 fi
