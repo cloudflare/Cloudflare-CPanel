b2ce64cabedc3fa61ca8f2f8e2dd3b0c7665e42c
Patrick R. Donahue <pat@cloudflare.com>
2015-06-16 16:32:30 -0400

    Download latest version (not master) when installing. Other minor tweaks.
    cloudflare.install.sh:
    - Check for effective UID of 0 before proceeding.
    - Use basename in usage() output.
    - Check Host API for current version and pull that (rather than pull
      master and check commit hash).
    - Extract tarball in place to obviate need to delete after.

--- cloudflare.install.sh
+++ cloudflare.install.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
+me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 host_key=$1
 formal_name=$2
 install_mode=$3
 
-echo "Starting CloudFlare CPanel Installation..."
+usage() {
+    echo "Usage: ./$me HOST_KEY 'YOUR_COMPANY_NAME' [install_mode]"
+    echo ""
+    echo "-      If you do not have a HOST_KEY, contact CloudFlare for one before proceeding."
+    echo -e "-      install_mode is optional. Set to 'simple' to have only 1 CloudFlare icon on the main cPanel dashboard.\n"
+    exit 1
+}
 
-echo "Downloading necessary files..."
-curl -k -L https://github.com/cloudflare/CloudFlare-CPanel/tarball/master > cloudflare.tmp.tar.gz
+# check that we're running as root (or "effectively" as root, i.e., euid=0)
+if [[ $EUID -ne 0 ]]; then
+  echo "You must run this installation script as root."
+  exit 1
+fi
 
-## Load the latest commit hash and pull out the first 7 characters
-sha=`curl -k -L https://api.github.com/repos/cloudflare/CloudFlare-CPanel/commits | grep -o -m1 '"sha": "[^"]*"'`
-commit_hash=${sha:8:7}
+# check parameters
+if [ -z "$host_key" ] || [ -z "$formal_name" ]; then
+  usage
+fi
 
-echo "Unpacking files..."
-tar -zxvf cloudflare.tmp.tar.gz
+echo -e "Starting CloudFlare CPanel Installation...\n"
 
-cd "cloudflare-CloudFlare-CPanel-${commit_hash}/cloudflare/"
+# find the proper version to download
+latest_version=$(curl -s https://api.cloudflare.com/host-gw.html -d "act=cpanel_info" -d "host_key=$host_key" | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep cpanel_latest | cut -d "\"" -f 6)
+
+if [ -z "$latest_version" ]; then
+  echo -e "Error: could not find latest version. Please double check your HOST_KEY.\n"
+  usage
+else
+  echo "Downloading and unpacking latest version v${latest_version}..." 
+fi
+
+# download and extract
+download_url="https://github.com/cloudflare/CloudFlare-CPanel/archive/v${latest_version}.tar.gz"
+curl -skL $download_url | tar xzf -
+
+cd "CloudFlare-CPanel-${latest_version}/cloudflare"
 
 echo "Finalizing install..."
-./install_cf "$1" mod_cf "$2" $3
-
-## Clean up files created by this install script
-echo "Removing temporary files..."
-cd ../../
-rm -rf "cloudflare.tmp.tar.gz"
+./install_cf "$host_key" mod_cf "$formal_name" $install_mode
 
 echo "Verify Host Name:"
 cat /usr/local/cpanel/etc/cloudflare.json
