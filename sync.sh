3f36dc67a54f59a0271e684fe2c5eb20c4a9fd16
John Wineman <johnwineman@Johns-MacBook-Pro-2.local>
2015-09-21 11:07:19 -0700

    Fixed bug where synch.sh was changing the owner/group of cfadmin causing it not to run correctly.

--- sync.sh
+++ sync.sh
@@ -2,10 +2,10 @@
 #$1 = username
 #$2 = hostname
 USRHOSTFLDR="$1@$2:/usr/local"
-rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./cloudflare/3rdparty/ $USRHOSTFLDR/cpanel/3rdparty
+rsync -avz --no-owner --no-group --no-perms -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./cloudflare/3rdparty/ $USRHOSTFLDR/cpanel/3rdparty
 #paper_lantern and x3 are symlinked to cf_base
-rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./cloudflare/base/frontend/cf_base/ $USRHOSTFLDR/cloudflare_cpanel/cf_base
-rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./cloudflare/bin/ $USRHOSTFLDR/cpanel/bin
-rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./cloudflare/Cpanel/ $USRHOSTFLDR/cpanel/Cpanel
-rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./cloudflare/etc/ $USRHOSTFLDR/cpanel/etc
-rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./cloudflare/src/ $USRHOSTFLDR/cpanel/src/wrap
+rsync -avz --no-owner --no-group --no-perms -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./cloudflare/base/frontend/cf_base/ $USRHOSTFLDR/cloudflare_cpanel/cf_base
+rsync -avz --no-owner --no-group --no-perms -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./cloudflare/bin/ $USRHOSTFLDR/cpanel/bin
+rsync -avz --no-owner --no-group --no-perms -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./cloudflare/Cpanel/ $USRHOSTFLDR/cpanel/Cpanel
+rsync -avz --no-owner --no-group --no-perms -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./cloudflare/etc/ $USRHOSTFLDR/cpanel/etc
+rsync -avz --no-owner --no-group --no-perms -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./cloudflare/src/ $USRHOSTFLDR/cpanel/src/wrap
