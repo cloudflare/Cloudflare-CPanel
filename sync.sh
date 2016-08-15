baed8e4cdbfa0936c25e96bf98310abe10438807
Furkan Yilmaz <mufuyil@gmail.com>
2016-08-15 18:05:40 +0100

    PI-721 update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,6 +3,7 @@
 #$1 = username
 #$2 = hostname
 USRHOSTFLDR="$1@$2:/usr/local"
+PHPVERSION="56"
 
 rsync -avz --no-owner --no-group --no-perms --copy-dirlinks -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./proxy.live.php $USRHOSTFLDR/cpanel/base/frontend/paper_lantern/cloudflare/proxy.live.php
 rsync -avz --no-owner --no-group --no-perms --copy-dirlinks -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./index.live.php $USRHOSTFLDR/cpanel/base/frontend/paper_lantern/cloudflare/index.live.php
@@ -15,5 +16,5 @@ rsync -avz --no-owner --no-group --no-perms --copy-dirlinks -e "ssh -o StrictHos
 rsync -avz --no-owner --no-group --no-perms --copy-dirlinks -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./lang/ $USRHOSTFLDR/cpanel/base/frontend/paper_lantern/cloudflare/lang
 rsync -avz --no-owner --no-group --no-perms --copy-dirlinks -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./CloudFlare.pm $USRHOSTFLDR/cpanel/Cpanel/API/CloudFlare.pm
 rsync -avz --no-owner --no-group --no-perms --copy-dirlinks -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./APIKey $USRHOSTFLDR/cpanel/bin/admin/CloudFlare/APIKey
-rsync -avz --no-owner --no-group --no-perms --copy-dirlinks -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./vendor/ $USRHOSTFLDR/cpanel/3rdparty/php/54/lib/php/cloudflare/vendor
-rsync -avz --no-owner --no-group --no-perms --copy-dirlinks -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./src/ $USRHOSTFLDR/cpanel/3rdparty/php/54/lib/php/cloudflare/src
+rsync -avz --no-owner --no-group --no-perms --copy-dirlinks -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./vendor/ $USRHOSTFLDR/cpanel/3rdparty/php/$PHPVERSION/lib/php/cloudflare/vendor
+rsync -avz --no-owner --no-group --no-perms --copy-dirlinks -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./src/ $USRHOSTFLDR/cpanel/3rdparty/php/$PHPVERSION/lib/php/cloudflare/src
