296196f94f895ecde4e879d3b8597ba05221ee87
Furkan Yilmaz <mufuyil@gmail.com>
2016-11-04 14:50:59 +0000

    DES-965 syntax cleanup

--- index.live.php
+++ index.live.php
@@ -1,10 +1,10 @@
-<?
-require_once("cloudflare/vendor/autoload.php");
-require_once("/usr/local/cpanel/php/cpanel.php");
+<?php
+require_once 'cloudflare/vendor/autoload.php';
+require_once '/usr/local/cpanel/php/cpanel.php';
 $cpanel = new CPANEL();
 
-$config = new CF\Integration\DefaultConfig(file_get_contents("config.js"));
-$logger = new CF\Integration\DefaultLogger($config->getValue("debug"));
+$config = new CF\Integration\DefaultConfig(file_get_contents('config.js'));
+$logger = new CF\Integration\DefaultLogger($config->getValue('debug'));
 $cpanelAPI = new CF\Cpanel\CpanelAPI($cpanel, $logger);
 $dataStore = new CF\Cpanel\DataStore($cpanelAPI, $logger);
 
@@ -13,32 +13,32 @@ $dataStore = new CF\Cpanel\DataStore($cpanelAPI, $logger);
  * to convert most people's YAML files.  No reason to support both formats indefinitely.
  */
 $uniqueId = $dataStore->getDeprecatedHostUserUniqueID();
-if($uniqueId) {
+if ($uniqueId) {
     $cpanelIntegration = new CF\Cpanel\CpanelIntegration($config, $cpanelAPI, $dataStore, $logger);
     $hostAPIClient = new CF\API\Host($cpanelIntegration);
-    $request = new \CF\API\Request('POST','', null, array('act' => 'user_lookup', 'unique_id' => $uniqueId));
+    $request = new \CF\API\Request('POST', '', null, array('act' => 'user_lookup', 'unique_id' => $uniqueId));
     $response = $hostAPIClient->callAPI($request);
-    if($hostAPIClient->responseOk($response)) {
+    if ($hostAPIClient->responseOk($response)) {
         $response = $response['response'];
-        $dataStore->createUserDataStore($response["user_api_key"], $response["cloudflare_email"], $response["unique_id"], $response["user_key"]);
+        $dataStore->createUserDataStore($response['user_api_key'], $response['cloudflare_email'], $response['unique_id'], $response['user_key']);
     } else {
-        $logger->error("Failed to convert the YAML file for user '". $cpanelAPI->getUserId() . "'");
+        $logger->error("Failed to convert the YAML file for user '".$cpanelAPI->getUserId()."'");
     }
 }
 
-$stylesheetsAndMetaTags = "
-    <meta http-equiv=\"cache-control\" content=\"max-age=0\" />
-    <meta http-equiv=\"cache-control\" content=\"no-cache\" />
-    <meta http-equiv=\"expires\" content=\"0\" />
-    <meta http-equiv=\"expires\" content=\"Tue, 01 Jan 1980 1:00:00 GMT\" />
-    <meta http-equiv=\"pragma\" content=\"no-cache\" />
-    <link rel=\"stylesheet\" href=\"./stylesheets/cf.core.css\" media=\"screen,projection\" charset=\"utf-8\"/>
-    <link rel=\"stylesheet\" href=\"./stylesheets/components.css\" media=\"screen,projection\" charset=\"utf-8\"/>
+$stylesheetsAndMetaTags = '
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <link rel="stylesheet" href="./stylesheets/cf.core.css" media="screen,projection" charset="utf-8"/>
+    <link rel="stylesheet" href="./stylesheets/components.css" media="screen,projection" charset="utf-8"/>
     <link rel="stylesheet" href="./stylesheets/plugins.css" media="screen,projection" charset="utf-8"/>;
     <link rel="stylesheet" href="./stylesheets/hacks.css" media="screen,projection" charset="utf-8"/>';
 
 //add our custom styles before the </head> ¯\_(ツ)_/¯
-$cpanelHeader = str_replace("</head>", $stylesheetsAndMetaTags."</head>", $cpanel->header("CloudFlare"));
+$cpanelHeader = str_replace('</head>', $stylesheetsAndMetaTags.'</head>', $cpanel->header('CloudFlare'));
 echo $cpanelHeader;
 ?>
 <script>
@@ -78,7 +78,7 @@ function RestProxyCallback(opts) {
 </script>
 <div id="root" class="cloudflare-partners site-wrapper"></div>
 <script src="compiled.js"></script>
-<?
+<?php
 $cpanel->footer();
 $cpanel->end();
 ?>
