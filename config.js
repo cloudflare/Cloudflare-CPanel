9fecff9da57a64fa3f70e64bac35ee61fd4caacc
Furkan Yilmaz <mufuyil@gmail.com>
2017-03-15 12:05:52 +0000

    PI-1046 remove unnecessary config options

--- config.js
+++ config.js
@@ -1,25 +1,26 @@
 {
-    "debug": false,
-    "featureManagerIsAlwaysOnlineEnabled": true,
-    "featureManagerIsBrowserCacheTTLEnabled": true,
-    "featureManagerIsBrowserIntegrityCheckEnabled": true,
-    "featureManagerIsCacheLevelEnabled": true,
-    "featureManagerIsChallengePassageEnabled": true,
-    "featureManagerIsDevelopmentModeEnabled": true,
-    "featureManagerIsFullZoneProvisioningEnabled": true,
-    "featureManagerIsIpv6Enabled": true,
-    "featureManagerIsMinifyEnabled": true,
-    "featureManagerIsPurgeCacheEnabled": true,
-    "featureManagerIsRailgunEnabled": true,
-    "featureManagerIsScanEnabled": false,
-    "featureManagerIsSecurityLevelEnabled": true,
-    "featureManagerIsSSLEnabled": false,
-    "useHostAPILogin": true,
-    "isDNSPageEnabled": true,
-    "homePageCards": ["AlwaysOnlineCard", "IPV6Card", "CacheLevelCard","RailgunCard","PurgeCacheCard"],
+    "debug": false, 
+    "featureManagerIsFullZoneProvisioningEnabled": true, 
+    "isDNSPageEnabled": true, 
+    "useHostAPILogin": true, 
+    "homePageCards": [
+        "AlwaysOnlineCard",
+        "IPV6Card", 
+        "CacheLevelCard",
+        "RailgunCard",
+        "PurgeCacheCard"
+    ],
     "moreSettingsCards": {
-        "container.moresettings.security": ["SecurityLevelCard", "ChallengePassageCard", "BrowserIntegrityCheckCard"],
-        "container.moresettings.speed": ["MinifyCard", "DevelopmentModeCard", "BrowserCacheTTLCard"]
+        "container.moresettings.security": [
+            "SecurityLevelCard", 
+            "ChallengePassageCard", 
+            "BrowserIntegrityCheckCard"
+        ],
+        "container.moresettings.speed": [
+            "MinifyCard", 
+            "DevelopmentModeCard", 
+            "BrowserCacheTTLCard"
+        ]
     },
     "locale": "en",
     "integrationName": "cpanel",
