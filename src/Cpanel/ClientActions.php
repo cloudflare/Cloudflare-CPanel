24d6c8a25a34a0b5f677114671dcc6a912796647
Furkan Yilmaz <mufuyil@gmail.com>
2017-06-22 15:39:53 +0100

    Recfactor ClientActions.php to use CamelCase

--- src/Cpanel/ClientActions.php
+++ src/Cpanel/ClientActions.php
@@ -48,37 +48,37 @@ class ClientActions
      */
     public function mergeCpanelAndCFDomains()
     {
-        $get_cpanel_domains = $this->cpanelAPI->getDomainList();
+        $getCpanelDomains = $this->cpanelAPI->getDomainList();
 
         //addon and primary domains are A records to the main_domain so for Cpanel its always a list of one
-        $cpanel_domain_list = array($get_cpanel_domains['main_domain']);
-        if (array_key_exists('addon_domains', $get_cpanel_domains) && !is_null($get_cpanel_domains['addon_domains'])) {
-            $cpanel_domain_list = array_merge($cpanel_domain_list, $get_cpanel_domains['addon_domains']);
+        $cpanelDomainList = array($getCpanelDomains['main_domain']);
+        if (array_key_exists('addon_domains', $getCpanelDomains) && !is_null($getCpanelDomains['addon_domains'])) {
+            $cpanelDomainList = array_merge($cpanelDomainList, $getCpanelDomains['addon_domains']);
         }
 
-        if (array_key_exists('parked_domains', $get_cpanel_domains) && !is_null($get_cpanel_domains['parked_domains'])) {
-            $cpanel_domain_list = array_merge($cpanel_domain_list, $get_cpanel_domains['parked_domains']);
+        if (array_key_exists('parked_domains', $getCpanelDomains) && !is_null($getCpanelDomains['parked_domains'])) {
+            $cpanelDomainList = array_merge($cpanelDomainList, $getCpanelDomains['parked_domains']);
         }
 
-        $merged_domain_list = array();
-        foreach ($cpanel_domain_list as $cpanel_domain) {
+        $mergedDomainList = array();
+        foreach ($cpanelDomainList as $cpanelDomain) {
             $found = false;
 
-            $request = new Request('GET', 'zones/', array('name' => $cpanel_domain), array());
-            $cpanel_zone = $this->api->callAPI($request);
+            $request = new Request('GET', 'zones/', array('name' => $cpanelDomain), array());
+            $cpanelZone = $this->api->callAPI($request);
 
-            if ($this->api->responseOk($cpanel_zone)) {
-                foreach ($cpanel_zone['result'] as $cf_zone) {
-                    if ($cf_zone['name'] === $cpanel_domain) {
+            if ($this->api->responseOk($cpanelZone)) {
+                foreach ($cpanelZone['result'] as $cfZone) {
+                    if ($cfZone['name'] === $cpanelDomain) {
                         $found = true;
-                        array_push($merged_domain_list, $cf_zone);
+                        array_push($mergedDomainList, $cfZone);
                     }
                 }
             }
 
             if ($found === false) {
-                array_push($merged_domain_list, array(
-                        'name' => $cpanel_domain,
+                array_push($mergedDomainList, array(
+                        'name' => $cpanelDomain,
                         'plan' => array('name' => ''),
                         'type' => '',
                         'status' => 'inactive',
@@ -86,13 +86,13 @@ class ClientActions
             }
         }
 
-        $cf_zones_list = array();
-        $cf_zones_list['result'] = $merged_domain_list;
-        $cf_zones_list['success'] = true;
+        $cfZonesList = array();
+        $cfZonesList['result'] = $mergedDomainList;
+        $cfZonesList['success'] = true;
 
-        $this->addSSLVerficiationDNSRecordForCName($merged_domain_list);
+        $this->addSSLVerficiationDNSRecordForCName($mergedDomainList);
 
-        return $cf_zones_list;
+        return $cfZonesList;
     }
 
     /**
@@ -160,26 +160,26 @@ class ClientActions
      */
     public function createDNSRecord()
     {
-        $create_dns_record_result = $this->api->callAPI($this->request);
-        if ($this->api->responseOk($create_dns_record_result)) {
-            $domain_name = $create_dns_record_result['result']['zone_name'];
+        $createDnsRecordResult = $this->api->callAPI($this->request);
+        if ($this->api->responseOk($createDnsRecordResult)) {
+            $domainName = $createDnsRecordResult['result']['zone_name'];
             /*
             * Only create the DNS record on CPanel if it was provisioned with CNAME setup, aka if the resolve-to DNS record is set.
             */
-            if ($this->partialZoneSet->getResolveToDNSRecord($this->cpanelAPI->getDNSRecords($domain_name)) !== null) {
+            if ($this->partialZoneSet->getResolveToDNSRecord($this->cpanelAPI->getDNSRecords($domainName)) !== null) {
                 //cpanel record names have trailing dot
-                $create_dns_record_result['result']['name'] = $this->cpanelAPI->getCpanelDnsRecordName($create_dns_record_result['result']['name']);
-                $sub_domain = $create_dns_record_result['result']['name'];
+                $createDnsRecordResult['result']['name'] = $this->cpanelAPI->getCpanelDnsRecordName($createDnsRecordResult['result']['name']);
+                $subDomain = $createDnsRecordResult['result']['name'];
 
-                if ($this->partialZoneSet->partialZoneSet($sub_domain, $domain_name)) {
-                    return $create_dns_record_result;
+                if ($this->partialZoneSet->partialZoneSet($subDomain, $domainName)) {
+                    return $createDnsRecordResult;
                 } else {
-                    return $this->api->createAPIError("Could not modify Cpanel DNS record for '".$sub_domain."', please contact your host.");
+                    return $this->api->createAPIError("Could not modify Cpanel DNS record for '".$subDomain."', please contact your host.");
                 }
             }
         }
 
-        return $create_dns_record_result;
+        return $createDnsRecordResult;
     }
 
     /**
@@ -189,12 +189,12 @@ class ClientActions
      */
     public function patchDNSRecord()
     {
-        $patch_dns_record_result = $this->api->callAPI($this->request);
-        if ($this->api->responseOk($patch_dns_record_result)) {
-            $patch_dns_record_result['result']['name'] = $this->cpanelAPI->getCpanelDnsRecordName($patch_dns_record_result['result']['name']);
+        $patchDnsRecordResult = $this->api->callAPI($this->request);
+        if ($this->api->responseOk($patchDnsRecordResult)) {
+            $patchDnsRecordResult['result']['name'] = $this->cpanelAPI->getCpanelDnsRecordName($patchDnsRecordResult['result']['name']);
         }
 
-        return $patch_dns_record_result;
+        return $patchDnsRecordResult;
     }
 
     /**
@@ -208,17 +208,17 @@ class ClientActions
             return $this->api->createAPIError(Partial::ADVANCED_ZONE_EDIT_DISABLED_ERROR);
         }
 
-        $path_array = explode('/', $this->request->getUrl());
-        $zone_tag = $path_array[1];
+        $pathArray = explode('/', $this->request->getUrl());
+        $zoneTag = $pathArray[1];
 
         //try to remove partial zone set up IF it exists
-        if ($zone_tag !== null) {
-            $zone_get_details_response = $this->api->zoneGetDetails($zone_tag);
-            if ($this->api->responseOk($zone_get_details_response)) {
-                $domain_name = $zone_get_details_response['result']['name'];
-                if ($domain_name !== null) {
-                    if ($this->partialZoneSet->removePartialZoneSet($domain_name) === false) {
-                        return $this->api->createAPIError("CPanel was unable to update the DNS records for '".$domain_name."' to no longer point at Cloudflare.  Please contact your host.");
+        if ($zoneTag !== null) {
+            $zoneGetDetailsResponse = $this->api->zoneGetDetails($zoneTag);
+            if ($this->api->responseOk($zoneGetDetailsResponse)) {
+                $domainName = $zoneGetDetailsResponse['result']['name'];
+                if ($domainName !== null) {
+                    if ($this->partialZoneSet->removePartialZoneSet($domainName) === false) {
+                        return $this->api->createAPIError("CPanel was unable to update the DNS records for '".$domainName."' to no longer point at Cloudflare.  Please contact your host.");
                     }
                 }
             }
@@ -234,63 +234,63 @@ class ClientActions
      */
     public function mergeDNSRecords()
     {
-        $cf_dns_record_list = $this->api->callAPI($this->request);
-        if (!$this->api->responseOk($cf_dns_record_list)) {
-            return $cf_dns_record_list;
+        $cfDnsRecordList = $this->api->callAPI($this->request);
+        if (!$this->api->responseOk($cfDnsRecordList)) {
+            return $cfDnsRecordList;
         }
 
-        $path_array = explode('/', $this->request->getUrl());
-        $zone_id = $path_array[1];
+        $pathArray = explode('/', $this->request->getUrl());
+        $zoneId = $pathArray[1];
 
-        $zone_get_details_response = $this->api->zoneGetDetails($zone_id);
-        if (!$this->api->responseOk($zone_get_details_response)) {
+        $zoneGetDetailsResponse = $this->api->zoneGetDetails($zoneId);
+        if (!$this->api->responseOk($zoneGetDetailsResponse)) {
             return $this->api->createAPIError('Could not merge DNS record because zone details could not be retrieved.');
         }
 
-        $domain_name = $zone_get_details_response['result']['name'];
+        $domainName = $zoneGetDetailsResponse['result']['name'];
 
-        $cpanel_dns_record_list = $this->cpanelAPI->getDNSRecords($domain_name);
+        $cpanelDnsRecordList = $this->cpanelAPI->getDNSRecords($domainName);
 
-        if (!$this->cpanelAPI->uapiResponseOk($cf_dns_record_list)) {
-            return $this->api->createAPIError("Error getting the DNS records for '".$domain_name."' from Cpanel.");
+        if (!$this->cpanelAPI->uapiResponseOk($cfDnsRecordList)) {
+            return $this->api->createAPIError("Error getting the DNS records for '".$domainName."' from Cpanel.");
         }
 
-        $cf_dns_record_name_list = array($this->partialZoneSet->getResolveToValue($domain_name));
+        $cfDnsRecordNameList = array($this->partialZoneSet->getResolveToValue($domainName));
 
         // The user cant provision the root domain or the resolve to record
         // so we add them to the name list to prevent them from being added
         // when we loop through the cpanel DNS records.
         // If the setup is full zone we want the record to be added to the
-        // cf_dns_record_list results.
-        if ($this->partialZoneSet->getResolveToDNSRecord($cpanel_dns_record_list) !== null) {
-            array_push($cf_dns_record_name_list, $domain_name.'.');
+        // cfDnsRecordList results.
+        if ($this->partialZoneSet->getResolveToDNSRecord($cpanelDnsRecordList) !== null) {
+            array_push($cfDnsRecordNameList, $domainName.'.');
         }
 
-        foreach ($cf_dns_record_list['result'] as $key => $cf_dns_record) {
+        foreach ($cfDnsRecordList['result'] as $key => $cfDnsRecord) {
             //add trailing dot to cf dns record names
-            $cf_dns_record_list['result'][$key]['name'] = $this->cpanelAPI->getCpanelDnsRecordName($cf_dns_record_list['result'][$key]['name']);
+            $cfDnsRecordList['result'][$key]['name'] = $this->cpanelAPI->getCpanelDnsRecordName($cfDnsRecordList['result'][$key]['name']);
 
             //build list of dns record names
-            array_push($cf_dns_record_name_list, $cf_dns_record_list['result'][$key]['name']);
+            array_push($cfDnsRecordNameList, $cfDnsRecordList['result'][$key]['name']);
         }
 
-        foreach ($cpanel_dns_record_list as $cpanel_dns_record) {
+        foreach ($cpanelDnsRecordList as $cpanelDnsRecord) {
             //if the record a type Cloudflare can proxy
-            if (!in_array(strtoupper($cpanel_dns_record->getType()), CpanelDNSRecord::$DNS_RECORDS_CF_CANNOT_PROXY)) {
+            if (!in_array(strtoupper($cpanelDnsRecord->getType()), CpanelDNSRecord::$DNS_RECORDS_CF_CANNOT_PROXY)) {
                 //if the cpanel record isn't in the array, add it.
-                if (!in_array($cpanel_dns_record->getName(), $cf_dns_record_name_list)) {
-                    array_push($cf_dns_record_list['result'], array(
-                        'name' => $cpanel_dns_record->getName(),
-                        'type' => $cpanel_dns_record->getType(),
-                        'content' => $cpanel_dns_record->getContent(),
-                        'ttl' => $cpanel_dns_record->getTtl(),
+                if (!in_array($cpanelDnsRecord->getName(), $cfDnsRecordNameList)) {
+                    array_push($cfDnsRecordList['result'], array(
+                        'name' => $cpanelDnsRecord->getName(),
+                        'type' => $cpanelDnsRecord->getType(),
+                        'content' => $cpanelDnsRecord->getContent(),
+                        'ttl' => $cpanelDnsRecord->getTtl(),
                         'proxied' => false,
-                        'zone_id' => $zone_id,
+                        'zone_id' => $zoneId,
                     ));
                 }
             }
         }
 
-        return $cf_dns_record_list;
+        return $cfDnsRecordList;
     }
 }
